8df4ebebb2ddb014c184698bb6cfc9b357994c6a
Trever Andersen <trever.r.andersen@gmail.com>
2018-01-26 19:28:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Code your solution in this file
 function findMatching(arr, str) {
   var newArr = arr.filter(function(str) {
-    return str == arr.str 
+    debugger;
+    return str == arr
   })
   console.log(newArr);
 }
