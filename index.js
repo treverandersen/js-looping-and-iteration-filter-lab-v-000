b31751792c3babd739e36c51769df4424e39d1c1
Trever Andersen <trever.r.andersen@gmail.com>
2018-01-26 19:45:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // Code your solution in this file
 function findMatching(arr, str) {
   return arr.filter(function(name) {
-    //debugger;
     return name.toLowerCase() === str.toLowerCase()
   })
-
 }
+
