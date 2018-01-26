022782345265096f9bd3f7c2c6c6c08cd8444d44
Trever Andersen <trever.r.andersen@gmail.com>
2018-01-26 21:49:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function findMatching(arr, str) {
 
 function fuzzyMatch(arr, letter) {
   return arr.filter(function(name) {
-    return letter === name.match(/^[a-z]/i)
+    return name.toLowerCase() === letter.toLowerCase()
   })
 }
 
