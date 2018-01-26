7a17fdb3833c97410f22a0da218d14b3846be99f
Trever Andersen <trever.r.andersen@gmail.com>
2018-01-26 19:32:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solution in this file
 function findMatching(arr, str) {
   var newArr = arr.filter(function(name) {
-    debugger;
-    return name.toLowerCase() === str 
+    //debugger;
+    return name.toLowerCase() === str
   })
   //console.log(newArr);
 }
